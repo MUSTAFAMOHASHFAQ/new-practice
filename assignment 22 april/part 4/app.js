80d74ae81b58def879a75b00bdd6a30ed18a8e99
MUSTAFAMOHASHFAQ <137651950+MUSTAFAMOHASHFAQ@users.noreply.github.com>
2023-11-25 04:56:31 +0500

    Add files via upload

---
+++ assignment 22 april/part 4/app.js
@@ -0,0 +1,15 @@
+let published = document.getElementById('submit')
+published.addEventListener('click',ShowMsg)
+
+function ShowMsg(){
+    alert("Published");
+}
+
+// function getValue(){
+//     let txt = document.getElementById('submit');
+//     let txtValue = txt.value;
+
+//     let result = document.getElementById('newBlogs');
+//     result.innerText = txtValue;
+    
+// }
